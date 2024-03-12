65a94388286aee9b94c87f1ca29182cc715aacca
Johan Toma <98798563+johan-toma@users.noreply.github.com>
2024-03-13 00:11:32 +0100

    feat: flask
    played around with flask tested displaying entire mongodb docs, some issues occur when trying to enter data inside the mongod from the storenode no idea how to resolve it sometimes it works sometimes itdoesnt.

---
+++ FlaskApp/flaskapp.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, render_template
+from pymongo import MongoClient
+import json
+from bson import json_util
+
+app = Flask(__name__)
+client = MongoClient("mongodb://localhost:27017/")
+db = client['weather_data']
+weather_data = db['weather_data']
+##main page used to redirect to the different functionalities
+@app.route('/')
+def index():
+    documents = list(weather_data.find({}))
+    jsonify_documents = json.loads(json_util.dumps(documents))
+    return jsonify(jsonify_documents)
+
+
+@app.route('/showtable')
+def showtable():
+    return ""
+
+
+@app.route('/displaygraph')
+def displaygraph():
+    return ""
+
+
+
+@app.route('/maxtemp')
+def maxtemp():
+    return ""
+
+
+@app.route('/avgtemp')
+def avgtemp():
+    return ""
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=8080)
+
