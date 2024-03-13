fb7af3a733cb1545133a71c3940d8031ee58e8a8
Johan Toma <98798563+johan-toma@users.noreply.github.com>
2024-03-13 22:04:11 +0100

    fix flaskapp
    created templates stored the showtable.html there since its by deauflt searching for a folder templates inside the flaskapp location, changed some logic inside the flask app added measurements collection

--- FlaskApp/flaskapp.py
+++ FlaskApp/flaskapp.py
@@ -5,22 +5,24 @@ from bson import json_util
 
 app = Flask(__name__)
 client = MongoClient("mongodb://localhost:27017/")
-db = client['weather_data']
-weather_data = db['weather_data']
-##main page used to redirect to the different functionalities
+db = client.weather_data
+measurements = db.measurements
+
+
+##Shows everythign stored inside the measurement collection in weather_data db. (this is justa test)
 @app.route('/')
 def index():
-    documents = list(weather_data.find({}))
+    documents = list(measurements.find({}))
     jsonify_documents = json.loads(json_util.dumps(documents))
     return jsonify(jsonify_documents)
 
 
 @app.route('/showtable')
 def showtable():
-    data=list(collection.find({}))
+    data=list(measurements.find({}))
     return render_template('showtable.html',data=data)
 
-
+'''
 @app.route('/displaygraph')
 def displaygraph():
     
@@ -52,7 +54,6 @@ def avgtemp():
 return render_template('avgtemp.html', average=average)
 
 
-
+'''
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=8080)
-
